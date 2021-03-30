c791e17175d451c080f80f1fd5406bc5546fe6f3
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-03-30 14:35:50 +0300

    Made changes to gridview builder in home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -30,32 +30,32 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        key: _scaffoldKey,
-        drawer: LeftDrawer(),
-        bottomNavigationBar: BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                icon: const Icon(Icons.menu_outlined),
-                iconSize: 30.0,
-                onPressed: () => _scaffoldKey.currentState!.openDrawer(),
-              ),
-            ],
-          ),
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: LeftDrawer(),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.menu_outlined),
+              iconSize: 30.0,
+              onPressed: () => _scaffoldKey.currentState!.openDrawer(),
+            ),
+          ],
         ),
-        body: HomePage(),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.pink,
-          onPressed: () => _addItem(context),
-          child: const Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
+      ),
+      body: SafeArea(
+        child: HomePage(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.pink,
+        onPressed: () => _addItem(context),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
       ),
     );
@@ -85,130 +85,122 @@ class HomePage extends StatelessWidget {
         if (snapshot.hasData) {
           if (snapshot.data != null) {
             return (snapshot.data!.docs.isNotEmpty)
-                ? ListView(
-                    children: [
-                      GridView.builder(
-                        itemCount: snapshot.data!.docs.length,
-                        shrinkWrap: true,
-                        padding: const EdgeInsets.only(
-                          left: 5,
-                          right: 5,
-                          top: 5,
-                        ),
-                        gridDelegate:
-                            const SliverGridDelegateWithMaxCrossAxisExtent(
-                          maxCrossAxisExtent: 350,
-                          crossAxisSpacing: 10,
-                          mainAxisSpacing: 10,
-                        ),
-                        itemBuilder: (context, index) {
-                          return ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: GridTile(
-                              footer: GridTileBar(
-                                backgroundColor: Colors.black38,
-                                trailing: Row(
-                                  children: [
-                                    IconButton(
-                                      icon: Icon(
-                                        Icons.delete_outline_outlined,
-                                        color: Colors.red,
-                                        size: 30,
-                                      ),
-                                      onPressed: () => showDialog(
-                                        context: context,
-                                        builder: (context) {
-                                          return AlertDialog(
-                                            title:
-                                                const Text('Confirm Delete'),
-                                            content: const Text(
-                                              'Do you really want to delete this product?',
-                                            ),
-                                            actions: [
-                                              OutlineButton(
-                                                onPressed: () =>
-                                                    Navigator.of(context)
-                                                        .pop(),
-                                                child: const Text('Cancel'),
-                                              ),
-                                              RaisedButton(
-                                                onPressed: () {
-                                                  snapshot.data!.docs[index]
-                                                      .reference
-                                                      .delete();
-                                                  Navigator.of(context).pop();
-                                                },
-                                                child: const Text(
-                                                  'Delete',
-                                                ),
-                                              ),
-                                            ],
-                                          );
-                                        },
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                title: Text(
-                                    '${snapshot.data!.docs[index].data()!['title']}'),
-                              ),
-                              child: GestureDetector(
-                                onTap: () => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => ItemDetail(
-                                      item: Item.fromJson(
-                                        snapshot.data!.docs[index].data()!,
-                                      ),
-                                      itemId: snapshot.data!.docs[index].id,
-                                    ),
+                ? GridView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.only(
+                      left: 5,
+                      right: 5,
+                    ),
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 250,
+                      childAspectRatio: 0.8,
+                    ),
+                    itemBuilder: (context, index) {
+                      return ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: GridTile(
+                          footer: GridTileBar(
+                            backgroundColor: Colors.black38,
+                            trailing: Row(
+                              children: [
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.delete_outline_outlined,
+                                    color: Colors.red,
+                                    size: 30,
                                   ),
-                                ),
-                                child: Card(
-                                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                  ),
-                                  child: CachedNetworkImage(
-                                    imageUrl: snapshot.data!.docs[index]
-                                        .data()!['images']
-                                        .first,
-                                    imageBuilder: (context, imageProvider) =>
-                                        Container(
-                                      decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                          image: imageProvider,
-                                          fit: BoxFit.cover,
-                                          alignment: Alignment.center,
-                                          colorFilter: ColorFilter.mode(
-                                            Colors.white,
-                                            BlendMode.colorBurn,
+                                  onPressed: () => showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text('Confirm Delete'),
+                                        content: const Text(
+                                          'Do you really want to delete this product?',
+                                        ),
+                                        actions: [
+                                          OutlineButton(
+                                            onPressed: () =>
+                                                Navigator.of(context).pop(),
+                                            child: const Text('Cancel'),
                                           ),
-                                        ),
-                                      ),
-                                    ),
-                                    placeholder: (context, url) =>
-                                        Shimmer.fromColors(
-                                      baseColor: Colors.grey[300]!,
-                                      highlightColor: Colors.grey[100]!,
-                                      child: Card(
-                                        child: Container(
-                                          width: double.infinity,
-                                          height: 300,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ),
-                                    errorWidget: (context, url, error) =>
-                                        const Icon(Icons.error),
+                                          RaisedButton(
+                                            onPressed: () {
+                                              snapshot
+                                                  .data!.docs[index].reference
+                                                  .delete();
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text(
+                                              'Delete',
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    },
                                   ),
+                                ),
+                              ],
+                            ),
+                            title: Text(
+                                '${snapshot.data!.docs[index].data()!['title']}'),
+                          ),
+                          child: GestureDetector(
+                            onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ItemDetail(
+                                  item: Item.fromJson(
+                                    snapshot.data!.docs[index].data()!,
+                                  ),
+                                  itemId: snapshot.data!.docs[index].id,
                                 ),
                               ),
                             ),
-                          );
-                        },
-                      ),
-                    ],
+                            child: Card(
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: CachedNetworkImage(
+                                imageUrl: snapshot.data!.docs[index]
+                                    .data()!['images']
+                                    .first,
+                                imageBuilder: (context, imageProvider) =>
+                                    Container(
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: imageProvider,
+                                      fit: BoxFit.cover,
+                                      alignment: Alignment.center,
+                                      colorFilter: ColorFilter.mode(
+                                        Colors.white,
+                                        BlendMode.colorBurn,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                placeholder: (context, url) =>
+                                    Shimmer.fromColors(
+                                  baseColor: Colors.grey[300]!,
+                                  highlightColor: Colors.grey[100]!,
+                                  child: Card(
+                                    child: Container(
+                                      width: double.infinity,
+                                      height: 300,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                errorWidget: (context, url, error) =>
+                                    const Icon(Icons.error),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   )
                 : const Center(
                     child: Text('No products posted yet'),
