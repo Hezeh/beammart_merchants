c8b8cd359cbcd4921aad93c3e80c730bf7f49b77
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-04-18 13:11:16 +0300

    Commented out analytics

--- lib/widgets/menu_list_tile_widget.dart
+++ lib/widgets/menu_list_tile_widget.dart
@@ -55,26 +55,26 @@ class MenuListTileWidget extends StatelessWidget {
           indent: 10.0,
           endIndent: 10.0,
         ),
-        ListTile(
-          leading: Icon(
-            Icons.analytics_outlined,
-            color: Colors.pink,
-          ),
-          title: Text(
-            'Analytics',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18.0,
-            ),
-          ),
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (_) => AnalyticsScreen(),
-              ),
-            );
-          },
-        ),
+        // ListTile(
+        //   leading: Icon(
+        //     Icons.analytics_outlined,
+        //     color: Colors.pink,
+        //   ),
+        //   title: Text(
+        //     'Analytics',
+        //     style: TextStyle(
+        //       fontWeight: FontWeight.bold,
+        //       fontSize: 18.0,
+        //     ),
+        //   ),
+        //   onTap: () {
+        //     Navigator.of(context).push(
+        //       MaterialPageRoute(
+        //         builder: (_) => AnalyticsScreen(),
+        //       ),
+        //     );
+        //   },
+        // ),
         Divider(
           color: Colors.pink,
           indent: 10.0,
