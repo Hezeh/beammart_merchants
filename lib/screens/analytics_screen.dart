fd2623b045fdf42faf141b03f3837577a3c04633
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-04-28 15:51:24 +0300

    Added pie charts

--- lib/screens/analytics_screen.dart
+++ lib/screens/analytics_screen.dart
@@ -3,7 +3,10 @@ import 'package:beammart_merchants/models/impressions_analytics_data.dart';
 import 'package:beammart_merchants/providers/subscriptions_provider.dart';
 import 'package:beammart_merchants/screens/payments_subscriptions_screen.dart';
 import 'package:beammart_merchants/services/analytics_service.dart';
+import 'package:beammart_merchants/widgets/indicator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -18,13 +21,76 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
   Widget build(BuildContext context) {
     final currentUser = FirebaseAuth.instance.currentUser;
     final subsProvider = Provider.of<SubscriptionsProvider>(context);
+    int? touchedIndex;
+
+    List<PieChartSectionData>? showingSections({
+      @required double? total,
+      @required double? search,
+      @required double? recs,
+      @required double? category,
+      @required double? profile,
+    }) {
+      return List.generate(4, (i) {
+        final isTouched = i == touchedIndex;
+        final double fontSize = isTouched ? 25 : 16;
+        final double radius = isTouched ? 60 : 50;
+        switch (i) {
+          case 0:
+            return PieChartSectionData(
+              color: const Color(0xff0293ee),
+              value: search,
+              title: "${((search! / total!) * 100).toStringAsFixed(0)}%",
+              radius: radius,
+              titleStyle: TextStyle(
+                  fontSize: fontSize,
+                  fontWeight: FontWeight.bold,
+                  color: const Color(0xffffffff)),
+            );
+          case 1:
+            return PieChartSectionData(
+              color: const Color(0xfff8b250),
+              value: recs,
+              title: "${((recs! / total!) * 100).toStringAsFixed(0)}%",
+              radius: radius,
+              titleStyle: TextStyle(
+                  fontSize: fontSize,
+                  fontWeight: FontWeight.bold,
+                  color: const Color(0xffffffff)),
+            );
+          case 2:
+            return PieChartSectionData(
+              color: const Color(0xff845bef),
+              value: category,
+              title: "${((category! / total!) * 100).toStringAsFixed(0)}%",
+              radius: radius,
+              titleStyle: TextStyle(
+                  fontSize: fontSize,
+                  fontWeight: FontWeight.bold,
+                  color: const Color(0xffffffff)),
+            );
+          case 3:
+            return PieChartSectionData(
+              color: const Color(0xff13d38e),
+              value: profile,
+              title: "${((profile! / total!) * 100).toStringAsFixed(0)}%",
+              radius: radius,
+              titleStyle: TextStyle(
+                  fontSize: fontSize,
+                  fontWeight: FontWeight.bold,
+                  color: const Color(0xffffffff)),
+            );
+          default:
+            return PieChartSectionData();
+        }
+      });
+    }
 
     Widget _buildImpressionsAnalytics(String _userId) {
       return Card(
         child: Container(
-          height: 250,
           child: ListView(
             physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
             children: [
               // Total Impressions
               Container(
@@ -68,7 +134,115 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                             trailing: Text(
                               "${snapshot.data!.categoryImpressions}",
                             ),
-                          )
+                          ),
+                          AspectRatio(
+                            aspectRatio: 1.3,
+                            child: Card(
+                              color: Colors.white,
+                              child: Row(
+                                children: <Widget>[
+                                  const SizedBox(
+                                    height: 18,
+                                  ),
+                                  Expanded(
+                                    child: AspectRatio(
+                                      aspectRatio: 1,
+                                      child: PieChart(
+                                        PieChartData(
+                                          pieTouchData: PieTouchData(
+                                              touchCallback:
+                                                  (pieTouchResponse) {
+                                            setState(() {
+                                              final desiredTouch =
+                                                  pieTouchResponse.touchInput
+                                                          is! PointerExitEvent &&
+                                                      pieTouchResponse
+                                                              .touchInput
+                                                          is! PointerUpEvent;
+                                              if (desiredTouch &&
+                                                  pieTouchResponse
+                                                          .touchedSection !=
+                                                      null) {
+                                                touchedIndex = pieTouchResponse
+                                                    .touchedSection!
+                                                    .touchedSectionIndex;
+                                              } else {
+                                                touchedIndex = -1;
+                                              }
+                                            });
+                                          }),
+                                          borderData: FlBorderData(
+                                            show: false,
+                                          ),
+                                          sectionsSpace: 0,
+                                          centerSpaceRadius: 40,
+                                          sections: showingSections(
+                                            total: snapshot
+                                                .data!.totalImpressions!
+                                                .ceilToDouble(),
+                                            category: snapshot
+                                                .data!.categoryImpressions!
+                                                .ceilToDouble(),
+                                            recs: snapshot.data!
+                                                .recommendationsImpressions!
+                                                .ceilToDouble(),
+                                            search: snapshot
+                                                .data!.searchImpressions!
+                                                .ceilToDouble(),
+                                            // TODO: Get Profile Impressions
+                                            profile: 10.0,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const <Widget>[
+                                      Indicator(
+                                        color: Color(0xff0293ee),
+                                        text: 'Search',
+                                        isSquare: true,
+                                      ),
+                                      SizedBox(
+                                        height: 4,
+                                      ),
+                                      Indicator(
+                                        color: Color(0xfff8b250),
+                                        text: 'Recs',
+                                        isSquare: true,
+                                      ),
+                                      SizedBox(
+                                        height: 4,
+                                      ),
+                                      Indicator(
+                                        color: Color(0xff845bef),
+                                        text: 'Category',
+                                        isSquare: true,
+                                      ),
+                                      SizedBox(
+                                        height: 4,
+                                      ),
+                                      Indicator(
+                                        color: Color(0xff13d38e),
+                                        text: 'Profile',
+                                        isSquare: true,
+                                      ),
+                                      SizedBox(
+                                        height: 18,
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    width: 28,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     );
@@ -99,9 +273,9 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
     Widget _buildClickAnalytics(String _userId) {
       return Card(
         child: Container(
-          height: 250,
           child: ListView(
             physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
             children: [
               // Total Impressions
               Container(
@@ -119,7 +293,6 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                     AsyncSnapshot<ClickAnalytics> snapshot) {
                   if (snapshot.hasData) {
                     return Container(
-                      // height: 250,
                       child: Column(
                         children: [
                           ListTile(
@@ -146,10 +319,117 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                               "${snapshot.data!.categoryClicks}",
                             ),
                           ),
-                           ListTile(
+                          ListTile(
                             title: Text('Profile Page Clicks'),
                             trailing: Text(
                               "${snapshot.data!.profileClicks}",
+                            ),
+                          ),
+                          AspectRatio(
+                            aspectRatio: 1.3,
+                            child: Card(
+                              color: Colors.white,
+                              child: Row(
+                                children: <Widget>[
+                                  const SizedBox(
+                                    height: 18,
+                                  ),
+                                  Expanded(
+                                    child: AspectRatio(
+                                      aspectRatio: 1,
+                                      child: PieChart(
+                                        PieChartData(
+                                          pieTouchData: PieTouchData(
+                                              touchCallback:
+                                                  (pieTouchResponse) {
+                                            setState(() {
+                                              final desiredTouch =
+                                                  pieTouchResponse.touchInput
+                                                          is! PointerExitEvent &&
+                                                      pieTouchResponse
+                                                              .touchInput
+                                                          is! PointerUpEvent;
+                                              if (desiredTouch &&
+                                                  pieTouchResponse
+                                                          .touchedSection !=
+                                                      null) {
+                                                touchedIndex = pieTouchResponse
+                                                    .touchedSection!
+                                                    .touchedSectionIndex;
+                                              } else {
+                                                touchedIndex = -1;
+                                              }
+                                            });
+                                          }),
+                                          borderData: FlBorderData(
+                                            show: false,
+                                          ),
+                                          sectionsSpace: 0,
+                                          centerSpaceRadius: 40,
+                                          sections: showingSections(
+                                            total: snapshot.data!.totalClicks!
+                                                .ceilToDouble(),
+                                            category: snapshot
+                                                .data!.categoryClicks!
+                                                .ceilToDouble(),
+                                            recs: snapshot
+                                                .data!.recommendationsClicks!
+                                                .ceilToDouble(),
+                                            search: snapshot.data!.searchClicks!
+                                                .ceilToDouble(),
+                                            profile: snapshot
+                                                .data!.profileClicks!
+                                                .ceilToDouble(),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const <Widget>[
+                                      Indicator(
+                                        color: Color(0xff0293ee),
+                                        text: 'Search',
+                                        isSquare: true,
+                                      ),
+                                      SizedBox(
+                                        height: 4,
+                                      ),
+                                      Indicator(
+                                        color: Color(0xfff8b250),
+                                        text: 'Recs',
+                                        isSquare: true,
+                                      ),
+                                      SizedBox(
+                                        height: 4,
+                                      ),
+                                      Indicator(
+                                        color: Color(0xff845bef),
+                                        text: 'Category',
+                                        isSquare: true,
+                                      ),
+                                      SizedBox(
+                                        height: 4,
+                                      ),
+                                      Indicator(
+                                        color: Color(0xff13d38e),
+                                        text: 'Profile',
+                                        isSquare: true,
+                                      ),
+                                      SizedBox(
+                                        height: 18,
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    width: 28,
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ],
